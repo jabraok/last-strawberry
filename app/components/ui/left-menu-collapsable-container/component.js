32a5a40b4448fa2c5470fc6df95a2a3f853aba03
Vinh Luu <vinhlt3@gmail.com>
2017-01-19 19:51:12 +0700

    add left-menu-collapsable-container component

--- app/components/ui/left-menu-collapsable-container/component.js
+++ app/components/ui/left-menu-collapsable-container/component.js
@@ -1,4 +1,18 @@
-import Ember from 'ember';
+import Ember from "ember";
+import computed from "ember-computed-decorators";
 
 export default Ember.Component.extend({
+  classNames: ["col", "stretch"],
+
+  @computed("isHidden")
+  cssClass(isHidden) {
+    let cssClass = "hide show-gt-sm";
+    return cssClass;
+  },
+
+  actions: {
+    toogle(){
+      this.set("isHidden", !this.get("isHidden"));
+    }
+  }
 });
