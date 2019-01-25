5b464b00c240a889a6b9a6975d9f3a76613c19e6
Vinh Luu <vinhlt3@gmail.com>
2019-01-25 23:26:53 +0700

    Fixed decorateComponentClass

--- tests/helpers/decorate-component-class.js
+++ tests/helpers/decorate-component-class.js
@@ -4,7 +4,7 @@ export default function decorateComponentClass() {
   Ember.Component.reopen({
     init() {
       this._super(arguments);
-      const debugName = this.constructor.toString().split(':')[1]
+      const debugName = this.toString().split(':')[1]
       const cssName = debugName.split('/').join('_');
       const newName = `debug_${cssName}`;
       if(!this.classNames.includes(newName)) {
