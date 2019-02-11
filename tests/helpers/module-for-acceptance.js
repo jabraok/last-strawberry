656e44108d153cfe0f74bbc3360c73e5fd6fff5e
Vinh Luu <vinhlt3@gmail.com>
2019-02-11 15:42:26 +0700

    Init upgrade Ember@3.7.1

--- tests/helpers/module-for-acceptance.js
+++ tests/helpers/module-for-acceptance.js
@@ -1,39 +1,39 @@
-import $ from 'jquery';
-import { module } from 'qunit';
-import { resolve } from 'rsvp';
-import startApp from '../helpers/start-app';
-import destroyApp from '../helpers/destroy-app';
-import { mockSetup, mockTeardown } from 'ember-data-factory-guy';
-import preferencesMock from '../mocks/preferences-service';
+// import $ from 'jquery';
+// import { module } from 'qunit';
+// import { resolve } from 'rsvp';
+// import startApp from '../helpers/start-app';
+// import destroyApp from '../helpers/destroy-app';
+// import { mockSetup, mockTeardown } from 'ember-data-factory-guy';
+// import preferencesMock from '../mocks/preferences-service';
 
-export default function(name, options = {}) {
-  module(name, {
-    beforeEach() {
+// export default function(name, options = {}) {
+//   module(name, {
+//     beforeEach() {
 
-      this.application = startApp();
+//       this.application = startApp();
 
-      // Mock preferencesService
-      this.application.register('service:mockPreferences', preferencesMock);
-      this.application.inject('component', 'preferencesService', 'service:mockPreferences');
+//       // Mock preferencesService
+//       this.application.register('service:mockPreferences', preferencesMock);
+//       this.application.inject('component', 'preferencesService', 'service:mockPreferences');
 
-      $.mockjax({ url: "https://andruxnet-random-famous-quotes.p.mashape.com*", responseText: '{"quote":"Houston, we have a problem.","author":"Apollo 13","category":"Movies"}', type: 'POST' });
+//       $.mockjax({ url: "https://andruxnet-random-famous-quotes.p.mashape.com*", responseText: '{"quote":"Houston, we have a problem.","author":"Apollo 13","category":"Movies"}', type: 'POST' });
 
-      mockSetup();
+//       mockSetup();
 
-      // Enable for mockjax logging
-      // $.mockjaxSettings.logging = true;
-      // $.mockjaxSettings.logging = 4;
+//       // Enable for mockjax logging
+//       // $.mockjaxSettings.logging = true;
+//       // $.mockjaxSettings.logging = 4;
 
-      if (options.beforeEach) {
-        return options.beforeEach.apply(this, arguments);
-      }
-    },
+//       if (options.beforeEach) {
+//         return options.beforeEach.apply(this, arguments);
+//       }
+//     },
 
-    afterEach() {
-      let afterEach = options.afterEach && options.afterEach.apply(this, arguments);
-      return resolve(afterEach)
-        .then(() => destroyApp(this.application))
-        .then(() => mockTeardown());
-    }
-  });
-}
+//     afterEach() {
+//       let afterEach = options.afterEach && options.afterEach.apply(this, arguments);
+//       return resolve(afterEach)
+//         .then(() => destroyApp(this.application))
+//         .then(() => mockTeardown());
+//     }
+//   });
+// }
