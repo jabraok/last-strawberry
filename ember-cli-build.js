57f30bdffcb7f0714b56258fb64677a2b206e42b
Vinh Luu <vinhlt3@gmail.com>
2019-02-12 14:18:27 +0700

    Upgrade ember-cli-sass

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,7 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
+    sassOptions: {implementation: require("node-sass")},
     babel: {
       optional: ['es7.decorators']
     },
