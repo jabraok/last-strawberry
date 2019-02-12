b5da34d5a1765946319eea2e44e4f3254cdf3435
Vinh Luu <vinhlt3@gmail.com>
2019-02-12 19:58:26 +0700

    Removed mockSetup and mockTeardown

--- tests/helpers/module-for-acceptance.js
+++ tests/helpers/module-for-acceptance.js
@@ -1,8 +1,6 @@
 import { module } from 'qunit';
-import { resolve } from 'rsvp';
 import startApp from '../helpers/start-app';
-import destroyApp from '../helpers/destroy-app';
-import { mockSetup, mockTeardown, getPretender } from 'ember-data-factory-guy';
+import { getPretender } from 'ember-data-factory-guy';
 import preferencesMock from '../mocks/preferences-service';
 
 export default function(name, options = {}) {
@@ -22,22 +20,9 @@ export default function(name, options = {}) {
 
       getPretender().get('https://api.mapbox.com/*', () => [200, {}, ""]);
 
-      mockSetup();
-
-      // Enable for mockjax logging
-      // $.mockjaxSettings.logging = true;
-      // $.mockjaxSettings.logging = 4;
-
       if (options.beforeEach) {
         return options.beforeEach.apply(this, arguments);
       }
-    },
-
-    afterEach() {
-      let afterEach = options.afterEach && options.afterEach.apply(this, arguments);
-      return resolve(afterEach)
-        .then(() => destroyApp(this.application))
-        .then(() => mockTeardown());
     }
-  });
+  })
 }
