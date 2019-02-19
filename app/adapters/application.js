fbd8ca8382cb7ebd4197ff7f26169787bb069311
Vinh Luu <vinhlt3@gmail.com>
2019-02-19 17:47:30 +0700

    Fixed deprecation devise Authorizer

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,8 +1,17 @@
 import DS from 'ember-data';
+import { inject as service } from '@ember/service';
 import config from 'last-strawberry/config/environment';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
   host: config.apiHost,
-  authorizer: 'authorizer:devise'
+  session: service(),
+  // defaults
+  // identificationAttributeName: 'email'
+  // tokenAttributeName: 'token'
+  authorize(xhr) {
+    let { email, token } = this.get('session.data.authenticated');
+    let authData = `Token token="${token}", email="${email}"`;
+    xhr.setRequestHeader('Authorization', authData);
+  }
 });
